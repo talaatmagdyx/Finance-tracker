4f6dae064577d1e9e60835543d3d02eb6721ca24
TalaatMagdy <talaatmagdy75@gmail.com>
2018-07-31 20:10:38 +0200

    slove again

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module FinanceTracker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+	config.assets.enabled = true
+
+	#config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
