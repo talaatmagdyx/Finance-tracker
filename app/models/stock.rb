8f5e722ad7eb42c8876112972814349b8577392b
TalaatMagdy <talaatmagdy75@gmail.com>
2018-08-04 19:29:44 +0200

    add Many to Many relation

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,8 @@
 class Stock < ApplicationRecord
 
+	has_many :user_stocks
+	has_many :users, through: :user_stocks
+
 	def self.new_form_lookup(ticker_symbol)
 		begin
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
 
   get 'search_stocks', to: 'stocks#search'
+
+  resources :user_stocks, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
