709adc4a9f272ecd300d94acafe20135d4bbfd6f
TalaatMagdy <talaatmagdy75@gmail.com>
2018-08-14 19:34:37 +0200

    update views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "user/registrations"}
+
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
 
