58628d6f7f797c24d53caab9081d799a6f52a8b8
TalaatMagdy <talaatmagdy75@gmail.com>
2018-08-18 06:34:17 +0200

    add friends and remove

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
   get 'my_friends', to: 'users#my_friends'
 
   get 'search_stocks', to: 'stocks#search'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: "users#add_friend"
 
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
